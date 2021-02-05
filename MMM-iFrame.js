9381d7f755cbcb75d0186eece07f3a43c780836b
Rivelia <kevisor@gmail.com>
2021-02-05 22:02:35 +0100

    no random

--- MMM-iFrame.js
+++ MMM-iFrame.js
@@ -7,51 +7,49 @@
  * MIT Licensed.
  */
 
-Module.register("MMM-iFrame",{
-		// Default module config.
-		defaults: {
-                                frameWidth: "300",
-				width:"100%",
-                                updateInterval: 0.5 * 60 * 1000,
-                                url: ["http://magicmirror.builders/"],
-                                scrolling: "no"
-		},
-
-        start: function () {
-                self = this;
-                var count = 0;
-                if (this.config.url.length > 0 ) {
-                      setInterval( function () { 
-                         self.updateDom(1000);
-                         console.log('update' + count++)
-                         }, this.config.updateInterval);
-                }
+Module.register('MMM-iFrame', {
+	// Default module config.
+	defaults: {
+		frameWidth: '300',
+		width: '100%',
+		updateInterval: 0.5 * 60 * 1000,
+		url: ['http://magicmirror.builders/'],
+		scrolling: 'no',
 	},
-  getRandomInt: function (min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  },
-resume: function() {
-   console.log("Resuming");
-   return this.getDom();
-},
-        getStyles: function() {
-                return [
-                        "MMM-iFrame.css",
-                ];
-        },
 
-        // Override dom generator.
-	getDom: function() {
-                var { width, height } = this.config;
-                var wrapper = document.createElement("div");
-                
-                wrapper.className = "mmm-iframe"
-                wrapper.style.width = `${this.config.frameWidth}px`;
+	start: function () {
+		self = this;
+		this.curIndex = 0;
+		if (this.config.url.length > 1) {
+			setInterval(function () {
+				self.updateDom(1000);
+			}, this.config.updateInterval);
+		}
+	},
+	resume: function () {
+		console.log('Resuming');
+		return this.getDom();
+	},
+	getStyles: function () {
+		return ['MMM-iFrame.css'];
+	},
 
-                var html = `
-                        <div class="mmm-iframe-wrapper" style="padding-top: ${100 / (width / height)}%;">
+	// Override dom generator.
+	getDom: function () {
+		var { width, height } = this.config;
+		var wrapper = document.createElement('div');
+
+		wrapper.className = 'mmm-iframe';
+		wrapper.style.width = `${this.config.frameWidth}px`;
+
+		this.curIndex = ++this.curIndex % this.config.url.length;
+
+		var html = `
+                        <div class="mmm-iframe-wrapper" style="padding-top: ${
+							100 / (width / height)
+						}%;">
                                 <iframe
-                                        src="${this.config.url[this.getRandomInt(0, this.config.url.length)]}"
+                                        src="${this.config.url[this.curIndex]}"
                                         width="${width}"
                                         height="${height}"
                                         scrolling="${this.config.scrolling}"
@@ -59,9 +57,8 @@ resume: function() {
                         </div>
                 `;
 
-                wrapper.insertAdjacentHTML("afterbegin", html);
+		wrapper.insertAdjacentHTML('afterbegin', html);
 
 		return wrapper;
-	}
-
+	},
 });
